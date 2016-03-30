6042faf31f39a56a16511c1b2293c4b7b913b26b
DesmondMM <mutsakaradesmond@gmail.com>
2016-03-30 12:11:19 +0200

    First commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from 'angular2/common';
+
+@component({
+    selector: 'demo-form-sku',
+    directives: [FORM_DIRECRTIVES],
+    template: `
+    <div class="ui raised segment">
+        <h2 class="ui header">Demo Form: SKU</h2>
+    </div>
+    `
+})
